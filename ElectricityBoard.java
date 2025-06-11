989558ec5084471e836f86aa6050a8a4bc5c27a9
KavyaG6 <112391394+KavyaG6@users.noreply.github.com>
2025-06-11 16:18:19 +0530

    Update ElectricityBoard.java

--- ElectricityBoard.java
+++ ElectricityBoard.java
@@ -1,6 +1,11 @@
 
 import java.util.*;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 public class ElectricityBoardMain {
 
     private Map<String, String> electricityMap;
